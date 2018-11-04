5695cabe7613c1c45e2f7153028397f9c31074c1
Sharanya <sharanyareddykr@gmail.com>
2018-11-04 10:53:36 -0800

    box_clarify_upadte_v1

--- routes/box+clarify.js
+++ routes/box+clarify.js
@@ -1,70 +1,67 @@
-const bodyParser = require('body-parser') // Body Parser for JSON encoded bodies
-const boxSDK = require('box-node-sdk');   // Box SDK
-const clarifai = require('clarifai');     // Clarifai SDK   // Keys and config
-const express = require('express')();     // Express
-const fs = require('fs');                 // File system access
-const http = require('http');             // HTTP server
-const util = require('util');
-const appConfig = require('./boxConfig.js');       // Auth keys and Box SDK
-const boxSDK = appConfig.boxSDK;
+'use strict';
 
-express.use(bodyParser.json());
-express.use(bodyParser.urlencoded({
-    extended: true
-}));
+// Initialize packages
+const util = require('util');            // Deep inspection of objects
+const boxSDK = require('box-node-sdk');  // Box SDK
+const fs = require('fs');                // File system for config
 
-// Create new Box SDK instance
+/**************************************************************
+ * Setup auth via downloaded JSON config file
+ **************************************************************/
+// Fetch config file for instantiating SDK instance
+// SAVE YOUR OWN APP CONFIG FILE TO config.json
+const configJSON = JSON.parse(fs.readFileSync('config.json'));
 
-const sdk = new boxSDK({
-    clientID: appConfig.oauthClientId,
-    clientSecret: appConfig.oauthClientSecret
-});
+// Instantiate instance of SDK using generated JSON config
+const sdk = boxSDK.getPreconfiguredInstance(configJSON);
 
-// Instantiate a new Clarifai app instance
-const app = new clarifai.App({
-    apiKey: '91d7b5baa0244d9c8400e0f4057cf5ea'
-});
+// Create service account client
+const client = sdk.getAppAuthClient('enterprise');
 
-client._session.getAccessToken().then(token => {
-    const fileURL = `https://api.box.com/2.0/files/${fileID}/content?access_token=${token}`;
+// Set service account to use as-user header
+client.asUser('6338445545');
 
-    // predict the contents of an image by passing in a url
-    app.models.predict(clarifai.GENERAL_MODEL, fileURL).then(
-        function(response) {
-            // Capture all categories
-            let entries = [];
-            for (let category of response.outputs[0].data.concepts) {
-                if (category.value > 0.9) {
-                    entries.push({ type: 'text', text: category.name });
-                }
-            }
+/**************************************************************
+ * Setup metadata information
+ **************************************************************/
+const fileId = '299905272211';
+const metadataTemplate = 'boxSkillsCards';
+const metadata = { cards: [{
+        created_at: new Date().toISOString(),
+        type: 'skill_card',
+        skill_card_type: 'keyword',
+        title: 'Topics',
+        skill: {
+            type: 'service',
+            id: 'box-skill-vb-audio-analysis-node'
+        },
+        invocation: {
+            type: 'skill_invocation',
+            id: fileId
+        },
+        entries: [{
+            type: 'text',
+            text: 'Keyword'
+        }]
+    }]};
 
+/**************************************************************
+ * Add or update metadata
+ **************************************************************/
+client.files.addMetadata(fileId, client.metadata.scopes.GLOBAL, metadataTemplate, metadata).then((err, metadata) => {
+    console.log("ADDING----------------------------------------------------------------");
+}).catch(function (err) {
+    if (err.response && err.response.body && err.response.body.code === 'tuple_already_exists') {
+        console.log("CONFLICT----------------------------------------------------------------");
 
-            // Set Box metadata template information
-            const metadataTemplate = 'boxSkillsCards';
-            const metadata = {
-                cards: [{
-                    created_at: new Date().toISOString(),
-                    type: 'skill_card',
-                    skill_card_type: 'keyword',
-                    skill_card_title: {
-                        message: 'Categories'
-                    },
-                    skill: {
-                        type: 'service',
-                        id: 'jleblanc-clarifai-heroku'
-                    },
-                    invocation: {
-                        type: 'skill_invocation',
-                        id: fileId
-                    },
-                    entries: entries
-                }]};
+        const jsonPatch = [{ op: 'replace', path: '/cards/0', value: metadata.cards[0] }];
 
+        client.files.updateMetadata(fileId, client.metadata.scopes.GLOBAL, metadataTemplate, jsonPatch).then((err, metadata) => {
+            console.log("UPDATED----------------------------------------------------------------");
+        }).catch(function (err) {
+            console.log(err.response.body);
         });
+    } else {
+        console.log(err.response.body);
+    }
 });
-            // Create server
-            const port = process.env.PORT || 3000;
-            http.createServer(express).listen(port, () => {
-                console.log(`Server started: Listening on port ${port}`);
-            });
